53449e669922896e1ff53bd3ca275392176b8bd6
AnthonyBeech <abeech123@virginmedia.com>
2023-12-30 12:29:30 +0000

    -

--- setup.py
+++ setup.py
@@ -2,19 +2,20 @@ from setuptools import find_packages, setup
 
 HYPHEN_E_DOT = "-e ."
 
-def get_requirements(file_path:str)->list[str]:
+
+def get_requirements(file_path: str) -> list[str]:
     """Func returns list of reqs"""
-    
-    requirements=[]
+
+    requirements = []
     with open(file_path) as file_obj:
         requirements = file_obj.readlines()
         requirements = [req.replace("\n", "") for req in requirements]
-        
+
         if HYPHEN_E_DOT in requirements:
             requirements.remove(HYPHEN_E_DOT)
-            
+
     return requirements
-            
+
 
 setup(
     name="mlproject",
@@ -22,5 +23,5 @@ setup(
     author="Beechhceeb",
     author_email="beechhceeb@gmail.com",
     packages=find_packages(),
-    install_requires=get_requirements("requirements.txt")
+    install_requires=get_requirements("requirements.txt"),
 )
