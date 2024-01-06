250095f9bea85dfda47bbce49020bb0af048057e
AnthonyBeech <abeech123@virginmedia.com>
2024-01-06 16:02:14 +0000

    -

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FPL MLOps
+
+# Summary
+* Project starts with legacy data from github found here [github](https://github.com/vaastav/Fantasy-Premier-League)
+    * Only game week data per player is used for now
+* 
--- src/pipeline/01_extract_players_from_legacy_github_data.py
+++ src/pipeline/01_extract_players_from_legacy_github_data.py
@@ -11,14 +11,17 @@ from src.components.data_ingestion import CsvExtractor
 class Config:
     year_min: int = 16
     year_max: int = 24
-    extracted_dir: str = "data/legacy/extracted/"  # Save csv files ectracted from legact data
-    combined_dir: str = "data/legacy/combined/" # Save csv files are combining by name
+    extracted_dir: str = "data/legacy/extracted/"  # location to save extracted gw data
+    combined_dir: str = "data/legacy/combined/" # location to save combined gw data
+    source_dir: str = "data/legacy/20yy_yy/players/"  # location to read legacy data
+
+
 
 
 cfg = Config
 
 if __name__ == "__main__":
-    logging.info("Extracting playeres from legacy github")
+    logging.info(f"Extracting gw data from leagacy github data in {cfg.source_dir} to {cfg.extracted_dir}")
 
     years = list(range(cfg.year_min, cfg.year_max))
 
@@ -30,12 +33,16 @@ if __name__ == "__main__":
 
     for year in years:
         try:
-            source_dir = f"data/legacy/20{year}-{year+1}/players/"
+            source_dir = cfg.source_dir.replace("yy_yy", f"{year}-{year+1}")
             extractor.extract_copy_csv(year, source_dir)
+            
         except Exception as e:
             logging.error(e)
 
     try:
+        logging.info(f"grouping files from {cfg.extracted_dir} to {cfg.combined_dir}")
         extractor.group_and_combine_files()
+        
     except Exception as e:
+        logging.error(e)
         raise CustomException(e)
